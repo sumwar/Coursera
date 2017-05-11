58cdabfe3272c1e8fe2f051f5a89542f87251176
DarkAce65 <tvdood99@gmail.com>
2017-05-11 10:30:15 -0400

    Structure project for gradle

---
+++ Algorithms 1/Percolation/src/main/java/Percolation.java
@@ -0,0 +1,29 @@
+public class Percolation {
+	public Percolation(int n) { // create n-by-n grid, with all sites blocked
+
+	}
+
+	public void open(int row, int col) { // open site (row, col) if it is not open already
+
+	}
+
+	public boolean isOpen(int row, int col) { // is site (row, col) open?
+		return false;
+	}
+
+	public boolean isFull(int row, int col) { // is site (row, col) full?
+		return false;
+	}
+
+	public int numberOfOpenSites() { // number of open sites
+		return 0;
+	}
+
+	public boolean percolates() { // does the system percolate?
+		return false;
+	}
+
+	public static void main(String[] args) { // test client (optional)
+
+	}
+}
